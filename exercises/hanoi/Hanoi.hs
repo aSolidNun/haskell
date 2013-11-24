601f0487f1a6b7fc40e24466814e4f9b2f1ed932
Ola Sundin <ola.sundin@me.com>
2013-11-24 12:45:09 +0100

    Added hanoi exercise

--- exercises/hanoi/Hanoi.hs
+++ exercises/hanoi/Hanoi.hs
@@ -3,3 +3,6 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 0 _ _ _ = []
+hanoi 1 a b c = (a, c):hanoi 0 a b c
+hanoi x a b c = hanoi (x - 1) a c b ++ [(a, c)] ++ hanoi (x - 1) b a c
